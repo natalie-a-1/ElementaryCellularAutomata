fbb91465519bf00b03cf2c809aba0506a53d3abb
Natalie Hill <natalie.a.hill-1@ou.edu>
2022-02-09 21:53:11 -0600

    Create Generation class

--- src/Generation.java
+++ src/Generation.java
@@ -1,4 +1,46 @@
 
 public class Generation {
 	private boolean[] cellStates;
+
+	public boolean[] getStates() {
+		return cellStates;
+	}
+
+	public boolean getState(int idx) {
+		return cellStates[idx];
+	}
+
+	public String getStates(char falseSymbol, char trueSymbol) {
+		return cellStates.toString();
+	}
+
+	public int size() {
+		return cellStates.length;
+	}
+
+	Generation(boolean... states) {
+		if (states == null || states.length == 0) {
+			cellStates[0] = false;
+		} else {
+			for (int i = 0; i < states.length; i++) {
+				cellStates[i] = states[i];
+			}
+		}
+
+	}
+
+	Generation(String states, char trueSymbol) {
+		char[] arr = states.toCharArray();
+		if (states == null || states.isEmpty()) {
+			cellStates[0] = false;
+		} else {
+			for (int i = 0; i < arr.length; i++) {
+				if (arr[i] == trueSymbol) {
+					cellStates[i] = true;
+				} else {
+					cellStates[i] = false;
+				}
+			}
+		}
+	}
 }
