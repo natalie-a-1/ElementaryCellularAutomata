431dcc130e0652a40690423b4140dc84953990f9
Natalie Hill <natalie.a.hill-1@ou.edu>
2022-02-09 23:34:37 -0600

    Write mthods for Automaton and Rule class

--- src/Automaton.java
+++ src/Automaton.java
@@ -1,4 +1,42 @@
+import java.util.ArrayList;
 
 public class Automaton {
+	private Rule rule;
+	private ArrayList<Generation> generations = new ArrayList<Generation>();
+	public char falseSymbol = '0';
+	public char trueSymbol = '1';
+	
+	public Generation getGeneration(int numSteps) {
+		
+	}
+	
+	public Generation getCurrentGeneration() {
+		
+	}
+	
+	public int getRuleNum() {
+		
+	}
+	
+	public int getTotalSteps() {
+		
+	}
+	
+	public void saveEvolution(String filename) {
+		
+	}
+	
+	public Automaton(int ruleNum, Generation intiial) {
+		
+	}
+	
+	public Automaton(String filename) {
+		
+	}
+	
+	public int evolve(int numSteps) {
+		
+	}
+	
+} 
 
-}
--- src/Rule.java
+++ src/Rule.java
@@ -1,4 +1,26 @@
 
 public class Rule {
-
+	private int wolfram_code;
+	
+	public ctor Rule(int ruleNum) {
+		if (ruleNum < 0 || ruleNum > 255) {
+			
+		}
+	}
+	
+	public int getRulNum() {
+		
+	}
+	
+	public boolean[] getNeighborhood(int idx, Generation gen) {
+		
+	}
+	
+	public boolean evolve(boolean[] neighborhood) {
+		
+	}
+	
+	public Generation evolve(Generation gen) {
+		
+	}
 }
