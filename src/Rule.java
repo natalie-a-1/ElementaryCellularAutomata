ca0aa14921c6f2889b4adb3d7aa459a1325e91ef
Natalie Hill <natalie.a.hill-1@ou.edu>
2022-02-15 18:05:11 -0600

    Create getNeighborhood method

--- src/Rule.java
+++ src/Rule.java
@@ -1,10 +1,15 @@
 
 public class Rule {
 	private int wolfram_code;
+	public int left_neighbor_state;
 	
-	public ctor Rule(int ruleNum) {
-		if (ruleNum < 0 || ruleNum > 255) {
-			
+	
+	
+	public Rule(int ruleNum) {
+		if (ruleNum < 0) {
+			ruleNum = 1;
+		} else if (ruleNum > 255) {
+			ruleNum = 255;
 		}
 	}
 	
@@ -13,7 +18,21 @@ public class Rule {
 	}
 	
 	public boolean[] getNeighborhood(int idx, Generation gen) {
-		
+		boolean[] temp = new boolean[2];
+		if (idx == 0) {
+			temp[0] = gen.getState(gen.size()-1);
+			temp[1] = gen.getState(idx);
+			temp[2] = gen.getState(idx + 1);
+		} else if (idx == gen.size() - 1) {
+			temp[0] = gen.getState(idx - 1);
+			temp[1] = gen.getState(idx);
+			temp[2] = gen.getState(0);
+		} else {
+			for (int i = idx - 1; i <= idx + 1; i++) {
+				temp[i] = gen.getState(i);
+			}
+		}
+		return temp;
 	}
 	
 	public boolean evolve(boolean[] neighborhood) {
@@ -21,6 +40,6 @@ public class Rule {
 	}
 	
 	public Generation evolve(Generation gen) {
-		
+		int[] states;
 	}
 }
