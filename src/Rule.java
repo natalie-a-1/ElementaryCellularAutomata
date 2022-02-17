d3f56692bdf60de2b808620807b263f130eeb3c1
Natalie Hill <natalie.a.hill-1@ou.edu>
2022-02-16 21:47:35 -0600

    Rule is a working progress right now

--- src/Rule.java
+++ src/Rule.java
@@ -1,24 +1,29 @@
 
+
 public class Rule {
 	private int wolfram_code;
-	public int left_neighbor_state;
+	private boolean[] temp;
+	private final int[] rule = {0,1,2,3,4,5,6,7};
 	
-	
-	
+	//convert to binary now?? Integer.toBinaryString(ruleNum);
 	public Rule(int ruleNum) {
+		String w_rule = "";
 		if (ruleNum < 0) {
 			ruleNum = 1;
-		} else if (ruleNum > 255) {
-			ruleNum = 255;
 		}
-	}
-	
-	public int getRulNum() {
+		if (ruleNum > 256) {
+			ruleNum = 256;
+		}
 		
+		w_rule = String.format("%8s", Integer.toBinaryString(ruleNum)).replace(' ', '0');
 	}
-	
-	public boolean[] getNeighborhood(int idx, Generation gen) {
-		boolean[] temp = new boolean[2];
+	//needs to return # of rule so rule method can determine state ?
+	public int getRuleNum() {
+		return wolfram_code;
+	}
+	//use toBinaryString??
+	public static boolean[] getNeighborhood(int idx, Generation gen) {
+		boolean[] temp = new boolean[gen.size()];
 		if (idx == 0) {
 			temp[0] = gen.getState(gen.size()-1);
 			temp[1] = gen.getState(idx);
@@ -34,12 +39,39 @@ public class Rule {
 		}
 		return temp;
 	}
-	
+	//return next state of a cell w given neighborhoods
 	public boolean evolve(boolean[] neighborhood) {
-		
+		for (int i = 0; i < temp.length; i++) {
+			if (neighborhood[0] && neighborhood[1] && neighborhood[2]) {
+				temp[i] = rule[0];
+			}
+			if (neighborhood[0] && neighborhood[1] && !neighborhood[2]) {
+				temp[i] = rule[1];
+			}
+			if (neighborhood[0] && !neighborhood[1] && neighborhood[2]) {
+				temp[i] = rule[2];
+			}
+			if (neighborhood[0] && !neighborhood[1] && !neighborhood[2]) {
+				temp[i] = rule[3];
+			}
+			if (!neighborhood[0] && neighborhood[1] && neighborhood[2]) {
+				temp[i] = rule[4];
+			}
+			if (!neighborhood[0] && neighborhood[1] && !neighborhood[2]) {
+				temp[i] = rule[5];
+			}
+			if (!neighborhood[0] && !neighborhood[1] && neighborhood[2]) {
+				temp[i] = rule[6];
+			}
+			if (!neighborhood[0] && !neighborhood[1] && !neighborhood[2]) {
+				temp[i] = rule[7];
+			}
+		}
+			
+		return temp[i];
 	}
 	
 	public Generation evolve(Generation gen) {
-		int[] states;
+		//apply rule to gen and return next generation
 	}
 }
