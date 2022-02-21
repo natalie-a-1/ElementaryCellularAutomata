928c554e81f5907b4f440ad3f99702c2dbdddefb
Natalie Hill <natalie.a.hill-1@ou.edu>
2022-02-20 18:30:07 -0600

    Working progress

--- src/ConsoleApp.java
+++ src/ConsoleApp.java
@@ -6,7 +6,7 @@ public class ConsoleApp {
 	 * NOTE: You do not need to modify any lines at or before 
 	 * the printOptions() method.
 	 */
-	
+
 	private static final Generation DEFAULT_GENERATION = new Generation(false, false, false, true, false, false, false);
 	private static final int DEFAULT_RULE = 22;
 
@@ -46,30 +46,30 @@ public class ConsoleApp {
 
 	private void processOption(Option option) {
 		switch (option) {
-			case PRINT_RULE:
-				printRule();
-				break;
-			case REINIT_AUTOMATON:
-				reinitAutomaton();
-				break;
-			case EVOLVE:
-				evolve();
-				break;
-			case SET_TRUE_SYMBOL:
-				setTrueSymbol();
-				break;
-			case SET_FALSE_SYMBOL:
-				setFalseSymbol();
-				break;
-			case PRINT_CURRENT_GENERATION:
-				printCurrentGeneration();
-				break;
-			case PRINT_FULL_EVOLUTION:
-				printFullEvolution();
-				break;
-			case QUIT:
-				printQuitMessage();
-				break;
+		case PRINT_RULE:
+			printRule();
+			break;
+		case REINIT_AUTOMATON:
+			reinitAutomaton();
+			break;
+		case EVOLVE:
+			evolve();
+			break;
+		case SET_TRUE_SYMBOL:
+			setTrueSymbol();
+			break;
+		case SET_FALSE_SYMBOL:
+			setFalseSymbol();
+			break;
+		case PRINT_CURRENT_GENERATION:
+			printCurrentGeneration();
+			break;
+		case PRINT_FULL_EVOLUTION:
+			printFullEvolution();
+			break;
+		case QUIT:
+			printQuitMessage();
+			break;
 		}
 	}
 
@@ -83,8 +83,8 @@ public class ConsoleApp {
 	/* You will need to implement the methods below. */
 
 	private void printRule() {
-		// TODO: Print out the current rule in the format specified by
-		// the README.
+
+		System.out.print("Rule: " + automaton.getRuleNum());
 	}
 
 	private void reinitAutomaton() {
@@ -97,24 +97,65 @@ public class ConsoleApp {
 	private void setRuleAndGeneration() {
 		// TODO: Prompt the user to enter a new rule number and initial
 		// generation. Refer to the README for details.
+
+		System.out.print("Enter new rule number > ");
+
+		int num = input.nextInt();
+
+		System.out.print("Enter initial generation > ");
+
+		String gen = input.next();
+
+		automaton = new Automaton(num, new Generation(gen, automaton.trueSymbol));
+		
+		System.out.println("Rule number updated to " + automaton.getRuleNum());
+		
+		printCurrentGeneration();
 	}
 
 	private void setTrueSymbol() {
 		// TODO: Prompts the user to enter a new true symbol representation.
+		System.out.println("Current true symbol: " + automaton.trueSymbol);
+
+		System.out.print("New true symbol > ");
+
+		String new_t = input.next();
+
+		char t = new_t.charAt(0);
+
+		automaton.trueSymbol = t;
 	}
 
 	private void setFalseSymbol() {
 		// TODO: Prompts the user to enter a new false symbol representation.
+		System.out.println("Current false symbol: " + automaton.falseSymbol);
+
+		System.out.print("New false symbol > ");
+
+		String new_f = input.next();
+
+		char f = new_f.charAt(0);
+
+		automaton.trueSymbol = f;
 	}
 
 	private void evolve() {
 		// TODO: Prompt the user to enter a number of evolutions and evolves
 		// the Automaton.
+		System.out.print("Enter number of evolutions > ");
+		
+		int numSteps = input.nextInt();
+		
+		automaton.evolve(numSteps);
+		
 	}
 
 	private void printCurrentGeneration() {
 		// TODO: Print the current generation of the Automaton using the
 		// Automaton's true and false symbols.
+		System.out.println("Generation " + automaton.getGeneration());
+		
+		System.out.print(automaton.getCurrentGeneration());
 	}
 
 	private void printFullEvolution() {
